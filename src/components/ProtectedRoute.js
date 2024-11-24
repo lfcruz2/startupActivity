d49843df9351d73b00378a47d7247c5da9a2b602
Macerla Avendaño <gmas48@hotmail.com>
2024-11-23 23:07:28 -0500

    Updates

--- src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ element: Component }) => {
-  const usuario = JSON.parse(localStorage.getItem('usuarioActivo'));
-  return usuario ? Component : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
